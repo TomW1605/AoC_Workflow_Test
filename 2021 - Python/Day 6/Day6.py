3b10a2299e939d24a6eb6b3e5456feb91087e530
Thomas White <tawhite@iinet.net.au>
2021-12-07 18:09:27 +0800

    add progress bar to day 6 brute force method

--- 2021 - Python/Day 6/Day6.py
+++ 2021 - Python/Day 6/Day6.py
@@ -1,18 +1,27 @@
 from collections import Counter
+from tqdm import tqdm
 
-from readFile import readFile
+#from readFile import readFile
+
+def readFile(file):
+    output = []
+    f = open(file, "r")
+    for x in f:
+        output.append(x.strip())
+    return output
 
 def part1(input_lines, days):
     #print(input_lines)
     fish = [int(item) for item in input_lines[0].split(",")]
-    for day in range(0, days):
-        for ii in range(0, len(fish)):
+    for day in tqdm(range(days), unit="day"):
+        for ii in tqdm(range(len(fish)), leave=False, unit="fish", unit_scale=True):
             if fish[ii] == 0:
                 fish[ii] = 6
                 fish.append(8)
             else:
                 fish[ii] -= 1
-        print("After", day, "days:", len(fish), "fish")# -", fish)
+        #print("\r")
+        #print("After", day, "days:", len(fish), "fish")# -", fish)
     print("After", days, "days:", len(fish), "fish")# -", fish)
 
 def part2(input_lines, days):
@@ -39,7 +48,7 @@ def part2(input_lines, days):
 
 if __name__ == '__main__':
     test = 0
-    part = 2
+    part = 1
     days = 256
 
     if test:
