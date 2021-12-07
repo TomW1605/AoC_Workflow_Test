a7f1b80cb082ff9fe706624d5adc267ead7d8ae4
TomW1605 <tawhite@iinet.net.au>
2021-12-07 15:54:47 +0800

    cleaned up day 7

--- 2021 - Python/Day 7/Day7.py
+++ 2021 - Python/Day 7/Day7.py
@@ -3,31 +3,41 @@ from readFile import readFile
 def part1(input_lines):
     #print(input_lines)
     crabs = [int(item) for item in input_lines[0].split(",")]
-    totalCosts = []
+    minFuel = 0
+    dest = 0
     for loc in range(min(crabs), max(crabs)+1):
         #print(loc)
-        totalCost = 0
+        totalFuel = 0
         for crab in crabs:
-            totalCost += abs(crab - loc)
-        totalCosts.append(totalCost)
-        print(loc, "-", totalCost)
-    print(totalCosts)
-    print(min(totalCosts))
+            totalFuel += abs(crab - loc)
+
+        if totalFuel < minFuel or minFuel == 0:
+            minFuel = totalFuel
+            dest = loc
+        else:
+            break
+        print(loc, "-", totalFuel)
+    print("cheapest dest:", dest, "-", minFuel)
 
 def part2(input_lines):
     #print(input_lines)
     crabs = [int(item) for item in input_lines[0].split(",")]
-    totalCosts = []
+    minFuel = 0
+    dest = 0
     for loc in range(min(crabs), max(crabs) + 1):
         #print(loc)
-        totalCost = 0
+        totalFuel = 0
         for crab in crabs:
             distance = abs(crab - loc)
-            totalCost += int((distance*(distance+1))/2)
-        totalCosts.append(totalCost)
-        print(loc, "-", totalCost)
-    #print(totalCosts)
-    print(min(totalCosts))
+            totalFuel += int((distance*(distance+1))/2)
+
+        if totalFuel < minFuel or minFuel == 0:
+            minFuel = totalFuel
+            dest = loc
+        else:
+            break
+        print(loc, "-", totalFuel)
+    print("cheapest dest:", dest, "-", minFuel)
 
 if __name__ == '__main__':
     test = 0
